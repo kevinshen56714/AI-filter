e680cdf3b42511f997774c6c090ce43bd9503ebb
kevinshen56714 <kshen64@gatech.edu>
2023-09-20 16:55:51 +0800

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google';
 
-import { Filters } from '@/types/filters';
 import { AIFilter } from '@/components/AIFilter';
+import mockMovies from '@/json/movies.json';
+import { Filters } from '@/types/filters';
+import { Movie } from '@/types/movie';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,9 +12,15 @@ export default function MovieList() {
     // Implement me! What should happen when the filters change?
   };
 
+  // Use our own movie type
+  const movies = mockMovies as Movie[];
+
   return (
     <main className={`flex min-h-screen flex-col items-center justify-start px-24 py-12 ${inter.className}`}>
       <h1 className="text-4xl font-bold text-center pb-12">Movies!</h1>
+      {movies.map((movie) => (
+        <p key={movie.slug}>{movie.title}</p>
+      ))}
       <AIFilter onFilterChange={onFilterChange} />
     </main>
   );
